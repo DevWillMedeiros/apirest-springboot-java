46ddf0930ee4aecc33c6f879a6d4a0011c4c9f0d
DevWillMedeiros <willmedeirospersonal@gmail.com>
2024-12-04 08:25:21 -0300

    primeiro commmit

---
+++ apirest-springboot-java/src/test/java/br/com/william/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.william;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
